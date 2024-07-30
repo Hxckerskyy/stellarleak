2ef3be9cec2506f7f2bb5e5f916b2bb329b7a197
Hxckerskyy <lajmicz2@gmail.com>
2024-07-30 22:42:54 +0200

    Update versions.lua

--- versions.lua
+++ versions.lua
@@ -2,4 +2,5 @@ return {
     --[[ [Da Hood](https://www.roblox.com/games/2788229376) ]]--
     [2788229376] = "(5cc74c)", -- Regular
     [16033173781] = "(5cc74c)", -- Macro
+    [18304546562] = "(5cc74c)", -- Macro
 }
