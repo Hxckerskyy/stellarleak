68348afc105cb7ac19ff4c55aa2c5d55fd538f31
Hxckerskyy <lajmicz2@gmail.com>
2024-07-31 11:40:11 +0200

    Create versv4.lua

---
+++ versv4.lua
@@ -0,0 +1,14 @@
+return {
+    --[[ [Da Hood](https://www.roblox.com/games/2788229376) ]]--
+    [2788229376] = "(5cc74c)", -- Regular
+    [16033173781] = "(5cc74c)", -- Macro
+    [18304546562] = "(20f8fb)", -- testing soms
+    [17897702920] = "(20f8fb)", -- og hood
+    [17344804827] = "(20f8fb)", -- yeno hood
+    [18461632007] = "(20f8fb)", -- der hood
+    [17723797487] = "(20f8fb)", -- dee hood
+    [15186202290] = "(20f8fb)", -- da strike
+    [17403265390] = "(20f8fb)", -- da downhill
+    [18111448661] = "(20f8fb)", -- da uphill
+    [14277620939] = "(b773fb)",-- custom ffa
+}
