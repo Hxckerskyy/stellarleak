4aecf5b8d22f43e2a349a243d928ea3edb75cd40
Hxckerskyy <lajmicz2@gmail.com>
2024-07-31 19:24:53 +0200

    Create dastrikesupport.lua

---
+++ dastrikesupport.lua
@@ -0,0 +1,7 @@
+return {
+    --[[ [Da Hood](https://www.roblox.com/games/2788229376) ]]--
+    [2788229376] = "(5cc74c)", -- Regular
+    [16033173781] = "(5cc74c)", -- Macro
+    [18304546562] = "(20f8fb)", -- testing soms
+    [15186202290] = "(20f8fb)", -- da strike
+}
