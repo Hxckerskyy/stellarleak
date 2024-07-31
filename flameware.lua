5af45b0eb4d2558d957cb15b828b2e1d2a267038
Hxckerskyy <lajmicz2@gmail.com>
2024-07-31 16:59:49 +0200

    Create flameware.lua

---
+++ flameware.lua
@@ -0,0 +1,7 @@
+return {
+    --[[ [Da Hood](https://www.roblox.com/games/2788229376) ]]--
+    [2788229376] = "(5cc74c)", -- Regular
+    [16033173781] = "(5cc74c)", -- Macro
+    [18304546562] = "(20f8fb)", -- testing soms
+    [15644861772] = "(20f8fb)", -- flameware
+}
